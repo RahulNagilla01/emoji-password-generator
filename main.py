1532b730e9f25219fbe21dadcbc1ae139ff2e9d8
okaGeek <rahulnagilla0@gmail.com>
2025-01-13 22:29:49 +0530

    Create main.py
    creating main body

---
+++ main.py
@@ -0,0 +1,9 @@
+import random
+import emoji
+import tkinter
+
+#Asking user for inputs 
+length_of_pass = int(input("Please give the length of password: "))
+
+#Creating a main GUI
+main_window = main_window.tk()
