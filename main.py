005b6f9903276fc0580773ade9be880ad1417cca
okaGeek <rahulnagilla0@gmail.com>
2025-01-25 19:24:50 +0530

    Update main.py
    final file of this project.

--- main.py
+++ main.py
@@ -1,9 +1,54 @@
 import random
-import emoji
-import tkinter
+Characters = "1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ~!@#$%^&*()_+"
+Faces = "🥸😏😶‍🌫️😒🙄😬🫣🤫🤔🫡🤐🤨😐😜😛😋🥲😙😚😊😗😘🤩😍🥰😇😀😃😄😁😆😅🤣😂🙂🙃🫠😉😊☹️😟🙁🫤😕🧐🤓😎🥸🥳🤯😵‍💫🤠😮😯😲😳🥺🥹😦😧😨😰😥😢😭😈🤬😠😡😤🥱😫😩😓😞😣😖😱👿💀☠️💩🤡👹👺"
+Fruits = "🍄🍇🍈🍉🍊🍋🍌🍍🥭🍎🍏🍐🍑🌶️🌽🥕🥔🍆🥑🥥🫒🍅🥝🫐🍓🍒🫑🥒🥬🥦🧄🧅🥜🫘🌰🍞🥐🍟🍔🥓🥩🍗🍖🧀🧇🥞🥯🫓🥖🍕🌭🥪🌮🌯🫔🥙🧆🥚🍳🥘🍲🫕🍝🍜🍛🍚🍙🍙🍘🧂🧈🍿🥣🥗🍠🍢🍣🍤🥮🍡🥟🥠🦀🦞🦐🍬🍫🥧🧁🍰🎂🍪🍩🍨🍧🍦🦪🦑🍭🍮🍯🍼🥛☕🫖🍵🍶🍾🍷🍸🍹"
+length_of_pass = int(input("Please Give the length of the password: "))
+All =  Faces + Fruits + Characters
 
-#Asking user for inputs 
-length_of_pass = int(input("Please give the length of password: "))
+list_of_sets = [Characters,Faces,Fruits,All]
 
-#Creating a main GUI
-main_window = main_window.tk()
+
+print("---------------------------------------------------------------------------------------------")
+print("setA 'Characters', ", Characters)
+print("---------------------------------------------------------------------------------------------")
+print("setB 'Faces', ", Faces)
+print("---------------------------------------------------------------------------------------------")
+print("setC 'Fruits', ", Fruits)
+print("---------------------------------------------------------------------------------------------")
+print("setD 'All', ", All)
+print("---------------------------------------------------------------------------------------------")
+
+try: 
+ choosen_set = str(input("Please choose one off the sets from above: "))
+
+except ValueError:
+    print("Please select a set from Faces, Characters, Fruits, All")
+
+if choosen_set == "Faces":
+    choosen_set = Faces
+elif choosen_set == "Characters":
+    choosen_set = Characters
+elif choosen_set == "Fruits":
+    choosen_set = Fruits
+elif choosen_set == "All":
+    choosen_set = All
+else:
+    print("Please choose one set from one set.")
+
+
+
+def password_func(length_of_pass):
+       main_password = []
+       for i in range(length_of_pass):
+            keys = random.choice(choosen_set)
+            main_password.append(keys)
+ 
+            password = "".join(main_password)
+            value = "The generated password is: "+ password
+
+       return print(value)
+
+password_func(length_of_pass)
+
+
+
